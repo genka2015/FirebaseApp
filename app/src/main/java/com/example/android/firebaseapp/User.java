eaef8c0331f1d12453dc60d2916b233c09d94a09
genka2015 <gem31@pitt.edu>
2017-06-08 12:42:02 -0400

    Make FireBase App

---
+++ app/src/main/java/com/example/android/firebaseapp/User.java
@@ -0,0 +1,42 @@
+package com.example.android.firebaseapp;
+
+
+
+public class User {
+
+    private String name;
+    private String email;
+
+    public User(){
+
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
